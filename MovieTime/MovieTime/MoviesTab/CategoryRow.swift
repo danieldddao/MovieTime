f4516fa9442ecc1f9111ff5ad8bff5563636c19b
jonhalverson1 <jonhalverson9@gmail.com>
2017-11-30 13:55:35 -0600

    Buffering Issue Fixed from Daniel's idea

--- MovieTime/MovieTime/MoviesTab/CategoryRow.swift
+++ MovieTime/MovieTime/MoviesTab/CategoryRow.swift
@@ -31,6 +31,7 @@ extension CategoryRow : UICollectionViewDataSource, UICollectionViewDelegate {
         
         let movie: MovieMDB
         movie = (genre?.movies[indexPath.row])!
+        cell.imgView.image = UIImage(named: "emptyPoster")
         
         if movie.poster_path != nil{
             //print("\(TMDBBase.imageURL)\(posterPath)")
